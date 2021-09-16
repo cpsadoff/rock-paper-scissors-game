97a070bd602dcd0c672969561973720cdc7768d8
cpsadoff <cps79@georgetown.edu>
2021-09-16 12:02:02 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,5 +5,18 @@ print("Rock, Paper, Scissors, Shoot!")
 #PROMPT USER FOR INPUT
 
 user_choice = input("Choose 'rock', 'paper', or 'scissors': ")
-print("You chose: ")
-print(user_choice)
+if (user_choice == "rock"):
+    print("You chose: ")
+    print(user_choice)
+elif (user_choice == "paper"):
+    print("You chose: ")
+    print(user_choice)
+elif (user_choice == "scissors"):
+    print("You chose: ")
+    print(user_choice)
+else :
+    print("Invalid choice.")
+    user_choice = input("Choose 'rock', 'paper', or 'scissors': ")
+
+#have to run this recursively
+
