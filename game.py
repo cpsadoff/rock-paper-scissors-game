c37b890ba21f7ead465c4ac923e84bfed40ce5be
cpsadoff <cps79@georgetown.edu>
2021-09-21 11:50:23 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,14 +6,16 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Choose 'rock', 'paper', or 'scissors': ")
 
-if (user_choice in ("rock", "paper", "scissors")):
+#CHECK USER INPUT
+
+if (user_choice in ("rock", "paper", "scissors")) :
     print("You chose: ", user_choice)
-    
+
 else :
     print("Invalid choice.")
     user_choice = input("Choose 'rock', 'paper', or 'scissors': ")
 
-#i want to run this recursively
+# i want to run this recursively
 
 #COMPUTER CHOICE (AT RANDOM)
 
@@ -22,3 +24,29 @@ options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
 print("Computer chose: ", computer_choice)
+
+#CALCULATING WINNER
+user_wins = "Congratulations! You won."
+computer_wins = "The computer won. Sorry :("
+tie = "It was a tie! Try again."
+
+if (user_choice == computer_choice) :
+    winner = tie
+elif ((user_choice == "rock") and (computer_choice == "scissors")) :
+    winner = user_wins
+elif ((user_choice == "rock") and (computer_choice == "paper")) :
+    winner = computer_wins
+elif ((user_choice == "paper") and (computer_choice == "scissors")) :
+    winner = computer_wins
+elif ((user_choice == "paper") and (computer_choice == "rock")) :
+    winner = user_wins
+elif ((user_choice == "scissors") and (computer_choice == "paper")) :
+    winner = user_wins
+elif ((user_choice == "scissors") and (computer_choice == "rock")) :
+    winner = computer_wins
+else :
+    winner = tie
+
+# if (winner = tie):
+
+print(winner)
